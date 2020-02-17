391b82d0a48deb673ea88db87117206d85c94c5e
Adam Porter <adam@alphapapa.net>
2020-02-17 02:43:19 -0600

    WIP

--- sbuffer.el
+++ sbuffer.el
@@ -31,6 +31,7 @@
 (require 'dash)
 (require 'f)
 (require 'magit-section)
+(require 'prism)
 
 ;;;; Variables
 
@@ -59,90 +60,89 @@
 
 (defun sbuffer ()
   (interactive)
-  (cl-labels ((group-buffers (buffers fns)
-                             (if (cdr fns)
-                                 (let ((groups (seq-group-by (car fns) buffers)))
-                                   (--map (cons (car it) (group-buffers (cdr it) (cdr fns)))
-                                          groups))
-                               (seq-group-by (car fns) buffers)))
-              (insert-thing (thing &optional (level 0))
-                            (pcase thing
-                              ((pred bufferp)
-                               (insert-buffer thing level))
-                              (_
-                               (insert-group thing level))))
-              (insert-buffer
-               (buffer level) (magit-insert-section nil (sbuffer-buffer buffer)
-                                (insert (make-string (* 2 level) ? ) (format-buffer buffer level) "\n")))
-              (insert-group
-               (group level) (pcase (car group)
-                               ('nil (pcase-let* ((`(,_type . ,things) group))
-                                       (--each things
-                                         (insert-thing it level))))
-                               (_ (pcase-let* ((`(,type . ,things) group))
-                                    (magit-insert-section (sbuffer-group type)
-                                      (magit-insert-heading (make-string (* 2 level) ? )
-                                        (format-group type level))
-                                      (--each things
-                                        (insert-thing it (1+ level))))))) )
-              (format-group
-               (group level) (propertize (cl-typecase group
-                                           (string group)
-                                           (otherwise (prin1-to-string group)))
-                                         'face (level-face level)))
-              (format-buffer
-               (buffer level) (propertize (buffer-name buffer)
-                                          'face (level-face level)))
-              (by-my-dirs
-               (buffer) (let ((buffer-dir (buffer-local-value 'default-directory buffer)))
-                          (or (cl-loop for test-dir in sbuffer-dirs
-                                       for matched-dir = (match-dir test-dir buffer-dir)
-                                       when matched-dir return matched-dir)
-                              "Paths")))
-              (match-dir (test-dir buffer-dir)
-                         (pcase test-dir
-                           (`(,(and (pred stringp)  test-dir (guard (dir-related-p test-dir buffer-dir))) ,depth)
-                            (apply #'f-join test-dir (-take depth (f-split (f-relative buffer-dir test-dir)))))
-                           ((pred stringp) (when (dir-related-p test-dir buffer-dir)
-                                             test-dir)))
-
-                         )
-              (dir-related-p (test-dir buffer-dir)
-                             (let ((test-dir (f-canonical test-dir))
-                                   (buffer-dir (f-canonical buffer-dir)))
-                               (or (f-equal? test-dir buffer-dir)
-                                   (f-ancestor-of? test-dir buffer-dir))))
-              (by-special-p
-               (buffer) (if (string-match-p (rx bos (optional (1+ blank)) "*") (buffer-name buffer))
-                            "*special*"
-                          "non-special buffers"))
-              (special-p
-               (buffer) (string-match-p (rx bos (optional (1+ blank)) "*") (buffer-name buffer)))
-              (by-hidden-p
-               (buffer) (if (string-prefix-p " " (buffer-name buffer))
-                            "*hidden*"
-                          "Normal"))
-              (by-indirect-p
-               (buffer) (when (buffer-base-buffer buffer)
-                          "*indirect*"))
-              (hidden-p
-               (buffer) (string-prefix-p " " (buffer-name buffer)))
-              (by-default-directory
-               (buffer) (propertize (file-truename (buffer-local-value 'default-directory buffer))
-                                    'face 'magit-section-heading))
-              (by-major-mode
-               (buffer) (propertize (symbol-name (buffer-local-value 'major-mode buffer))
-                                    'face 'magit-head))
-              (as-string
-               (arg) (cl-typecase arg
-                       (string arg)
-                       (otherwise (format "%s" arg))))
-              (format< (test-dir buffer-dir) (string< (as-string test-dir) (as-string buffer-dir)))
-              (boring-p (buffer)
-                        (or (special-p buffer)
-                            (hidden-p buffer)))
-              (level-face
-               (level) (intern (format "prism-level-%s" level))))
+  (cl-labels
+      ((group-buffers (buffers fns)
+                      (if (cdr fns)
+                          (let ((groups (seq-group-by (car fns) buffers)))
+                            (--map (cons (car it) (group-buffers (cdr it) (cdr fns)))
+                                   groups))
+                        (seq-group-by (car fns) buffers)))
+       (insert-thing (thing &optional (level 0))
+                     (pcase thing
+                       ((pred bufferp)
+                        (insert-buffer thing level))
+                       (_
+                        (insert-group thing level))))
+       (insert-buffer
+        (buffer level) (magit-insert-section nil (sbuffer-buffer buffer)
+                         (insert (make-string (* 2 level) ? ) (format-buffer buffer level) "\n")))
+       (insert-group
+        (group level) (pcase (car group)
+                        ('nil (pcase-let* ((`(,_type . ,things) group))
+                                (--each things
+                                  (insert-thing it level))))
+                        (_ (pcase-let* ((`(,type . ,things) group))
+                             (magit-insert-section (sbuffer-group type)
+                               (magit-insert-heading (make-string (* 2 level) ? )
+                                 (format-group type level))
+                               (--each things
+                                 (insert-thing it (1+ level))))))) )
+       (format-group
+        (group level) (propertize (cl-typecase group
+                                    (string group)
+                                    (otherwise (prin1-to-string group)))
+                                  'face (level-face level)))
+       (format-buffer
+        (buffer level) (propertize (buffer-name buffer)
+                                   'face (level-face level)))
+       (by-my-dirs
+        (buffer) (let ((buffer-dir (buffer-local-value 'default-directory buffer)))
+                   (or (cl-loop for test-dir in sbuffer-dirs
+                                for matched-dir = (match-dir test-dir buffer-dir)
+                                when matched-dir return matched-dir)
+                       "Paths")))
+       (match-dir (test-dir buffer-dir)
+                  (pcase test-dir
+                    (`(,(and (pred stringp)  test-dir (guard (dir-related-p test-dir buffer-dir))) ,depth)
+                     (apply #'f-join test-dir (-take depth (f-split (f-relative buffer-dir test-dir)))))
+                    ((pred stringp) (when (dir-related-p test-dir buffer-dir)
+                                      test-dir))))
+       (dir-related-p (test-dir buffer-dir)
+                      (let ((test-dir (f-canonical test-dir))
+                            (buffer-dir (f-canonical buffer-dir)))
+                        (or (f-equal? test-dir buffer-dir)
+                            (f-ancestor-of? test-dir buffer-dir))))
+       (by-special-p
+        (buffer) (if (string-match-p (rx bos (optional (1+ blank)) "*") (buffer-name buffer))
+                     "*special*"
+                   "non-special buffers"))
+       (special-p
+        (buffer) (string-match-p (rx bos (optional (1+ blank)) "*") (buffer-name buffer)))
+       (by-hidden-p
+        (buffer) (if (string-prefix-p " " (buffer-name buffer))
+                     "*hidden*"
+                   "Normal"))
+       (by-indirect-p
+        (buffer) (when (buffer-base-buffer buffer)
+                   "*indirect*"))
+       (hidden-p
+        (buffer) (string-prefix-p " " (buffer-name buffer)))
+       (by-default-directory
+        (buffer) (propertize (file-truename (buffer-local-value 'default-directory buffer))
+                             'face 'magit-section-heading))
+       (by-major-mode
+        (buffer) (propertize (symbol-name (buffer-local-value 'major-mode buffer))
+                             'face 'magit-head))
+       (as-string
+        (arg) (cl-typecase arg
+                (string arg)
+                (otherwise (format "%s" arg))))
+       (format< (test-dir buffer-dir) (string< (as-string test-dir) (as-string buffer-dir)))
+       (boring-p (buffer)
+                 (or (special-p buffer)
+                     (hidden-p buffer)))
+       (level-face
+        (level) (intern (format "prism-level-%s" level))))
     (with-current-buffer (get-buffer-create "*SBuffer*")
       (let* ((inhibit-read-only t)
              (group-fns (list #'by-my-dirs #'by-default-directory #'by-indirect-p #'by-major-mode))
