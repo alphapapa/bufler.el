a484561fe2cfccb1c7b08bf27451c671e843c9a8
Adam Porter <adam@alphapapa.net>
2020-04-01 07:15:15 -0500

    Meta: (.dir-locals.el) Set indent-tabs-mode nil
    Since I had worked on this in another Emacs config that didn't have
    this set, a bunch of tabs crept into the file!

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,7 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-status-mode
+((emacs-lisp-mode
+  (indent-tabs-mode))
+ (magit-status-mode
   (magit-todos-exclude-globs "makem.sh" "Makefile")))
