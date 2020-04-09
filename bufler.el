b2b260e4f9e8ba76bb8b4d71344c7b75e05ac44f
Adam Porter <adam@alphapapa.net>
2020-04-09 07:53:34 -0500

    Meta: 0.3-pre

--- bufler.el
+++ bufler.el
@@ -4,7 +4,7 @@
 
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; URL: https://github.com/alphapapa/bufler.el
-;; Package-Version: 0.2
+;; Package-Version: 0.3-pre
 ;; Package-Requires: ((emacs "26.3") (dash "2.17") (dash-functional "2.17") (f "0.17") (pretty-hydra "0.2.2") (magit-section "0.1"))
 ;; Keywords: convenience
 
