a7650d8e8561c0f03552dd129249199cd99b621f
Gerry Agbobada <10496163+gagbo@users.noreply.github.com>
2021-03-24 23:07:21 +0100

    Change required emacs version to 27
    Because of the use of group-name, which got added in 27.1

--- bufler.el
+++ bufler.el
@@ -5,7 +5,7 @@
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; URL: https://github.com/alphapapa/bufler.el
 ;; Package-Version: 0.3-pre
-;; Package-Requires: ((emacs "26.3") (dash "2.17") (dash-functional "2.17") (f "0.17") (pretty-hydra "0.2.2") (magit-section "0.1"))
+;; Package-Requires: ((emacs "27.1") (dash "2.17") (dash-functional "2.17") (f "0.17") (pretty-hydra "0.2.2") (magit-section "0.1"))
 ;; Keywords: convenience
 
 ;;; License:
