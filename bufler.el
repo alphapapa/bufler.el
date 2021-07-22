b951e621bc4a4bb07babf8b32dc318d91ae261c9
Adam Porter <adam@alphapapa.net>
2021-07-22 12:03:24 -0500

    Fix: Depend on map 2.1
    Fixes #70.  Thanks to @rmolinari for reporting.

--- bufler.el
+++ bufler.el
@@ -5,7 +5,7 @@
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; URL: https://github.com/alphapapa/bufler.el
 ;; Package-Version: 0.3-pre
-;; Package-Requires: ((emacs "26.3") (dash "2.18") (f "0.17") (pretty-hydra "0.2.2") (magit-section "0.1"))
+;; Package-Requires: ((emacs "26.3") (dash "2.18") (f "0.17") (pretty-hydra "0.2.2") (magit-section "0.1") (map "2.1"))
 ;; Keywords: convenience
 
 ;;; License:
