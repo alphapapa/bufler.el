bf7c4c4e213e1fb83689fba693389b745fbdea1d
Adam Porter <adam@alphapapa.net>
2021-09-13 23:21:51 +0000

    WIP: Updates for taxy changes

--- bufler-taxy.el
+++ bufler-taxy.el
@@ -53,11 +53,11 @@ example, setting to -1 prevents indentation of the first and
 second levels."
   :type 'integer)
 
-(defcustom bufler-taxy-level-indent 1
+(defcustom bufler-taxy-level-indent 2
   "Indentation per level of depth."
   :type 'integer)
 
-(defcustom bufler-taxy-item-indent 1
+(defcustom bufler-taxy-item-indent 2
   "Indentation of items relative to their level's indentation."
   :type 'integer)
 
@@ -73,24 +73,10 @@ second levels."
 
 ;;;; Macros
 
-(defmacro bufler-taxy-define-key (name args &rest body)
-  "Define a `bufler-taxy' key function by NAME having BODY taking ARGS.
-Within BODY, `buffer' is bound to the buffer being tested.
-
-Defines a `cl-function' named `bufler-taxy--predicate-NAME', and
-adds an entry to `bufler-taxy-keys' mapping NAME to the new
-function symbol."
-  (declare (indent defun)
-	   (debug (&define symbolp listp &rest def-form)))
-  (let* ((fn-symbol (intern (format "bufler-taxy--predicate-%s" name)))
-	 (fn `(cl-function
-               (lambda (buffer ,@args)
-                 ,@body))))
-    `(progn
-       (fset ',fn-symbol ,fn)
-       (setf (map-elt bufler-taxy-keys ',name) ',fn-symbol))))
-
 ;;;; Keys
+
+(taxy-define-key-definer bufler-taxy-define-key
+  bufler-taxy-keys "bufler-taxy" "FIXME: Docstring.")
 
 (bufler-taxy-define-key directory (&optional directory &key descendant-p name)
   "Return key string for BUFFER's directory, or nil.
@@ -101,14 +87,14 @@ DIRECTORY.  DIRECTORY should end in a slash."
   ;; It seems like undesirable overhead to `file-truename' every
   ;; time this function is called, but avoiding that wouldn't be easy.
   (pcase directory
-    ('nil (concat "Directory: " (buffer-local-value 'default-directory buffer)))
+    ('nil (concat "Directory: " (buffer-local-value 'default-directory item)))
     (_
      (cl-assert (directory-name-p directory) t "DIRECTORY should end in a directory separator character (i.e. a slash)")
      (setf directory (file-truename directory))
      (pcase descendant-p
-       ('nil (when (equal directory (file-truename (buffer-local-value 'default-directory buffer)))
+       ('nil (when (equal directory (file-truename (buffer-local-value 'default-directory item)))
                (or name (concat "Directory: " directory))))
-       (_ (when (string-prefix-p directory (file-truename (buffer-local-value 'default-directory buffer)))
+       (_ (when (string-prefix-p directory (file-truename (buffer-local-value 'default-directory item)))
             (or name (concat "Directory: " directory))))))))
 
 (bufler-taxy-define-key mode (&key mode regexp name)
@@ -117,22 +103,22 @@ If MODE, return key string if BUFFER's mode is MODE.  If REGEXP,
 return key string if BUFFER's mode name matches REGEXP.  If NAME,
 return that string as the key when BUFFER matches."
   (cond
-   (mode (when (eq mode (buffer-local-value 'major-mode buffer))
-           (or name (symbol-name (buffer-local-value 'major-mode buffer)))))
-   (regexp (when (string-match-p regexp (symbol-name (buffer-local-value 'major-mode buffer)))
+   (mode (when (eq mode (buffer-local-value 'major-mode item))
+           (or name (symbol-name (buffer-local-value 'major-mode item)))))
+   (regexp (when (string-match-p regexp (symbol-name (buffer-local-value 'major-mode item)))
              (or name (format "Mode: matches %s" regexp))))
-   (t (symbol-name (buffer-local-value 'major-mode buffer)))))
+   (t (symbol-name (buffer-local-value 'major-mode item)))))
 
 (bufler-taxy-define-key project ()
   "Return key string for BUFFER's project, or nil."
-  (when-let* ((project (with-current-buffer buffer
+  (when-let* ((project (with-current-buffer item
                          (project-current)))
               (project-root (bufler-project-root project)))
     (concat "Project: " project-root)))
 
 (bufler-taxy-define-key parent-project ()
   "Return key string for BUFFER's parent project, or nil."
-  (when-let* ((project (project-current nil (buffer-local-value 'default-directory buffer))))
+  (when-let* ((project (project-current nil (buffer-local-value 'default-directory item))))
     (let* ((project-root (bufler-project-root project))
            (parent-dir (file-name-directory (directory-file-name project-root)))
            (parent-dir-project (project-current nil parent-dir)))
@@ -142,46 +128,73 @@ return that string as the key when BUFFER matches."
                 project-root)))))
 
 (bufler-taxy-define-key special (&key (name "*special*"))
-  "Return NAME if BUFFER is special.
+  "Return NAME if buffer ITEM is special.
 A buffer is special if it is not file-backed."
-  (unless (buffer-file-name buffer)
+  (unless (buffer-file-name item)
     name))
 
 (bufler-taxy-define-key indirect (&key (name "*indirect*"))
-  "Return NAME if BUFFER is indirect."
-  (when (buffer-base-buffer buffer)
+  "Return NAME if buffer ITEM is indirect."
+  (when (buffer-base-buffer item)
     name))
 
 (defvar bufler-taxy-default-keys
-  '((parent-project special mode)
-    (special mode)
+  '((parent-project (special) mode)
+    (special)
     directory
     mode)
   "Default key functions.")
 
 ;;;; Columns
 
-(bufler-define-column "Name" (:max-width nil)
-  ;; MAYBE: Move indentation back to `bufler-list'.  But this seems to
-  ;; work well, and that might be more complicated.
-  ;; FIXME: Remove or change DEPTH argument in `bufler-define-column'.
-  (let ((indentation (make-string (+ (* depth bufler-taxy-level-indent)
-                                     bufler-taxy-item-indent)
-                                  ? ))
-        (mode-annotation (when (cl-loop for fn in bufler-buffer-mode-annotate-preds
-                                        thereis (funcall fn buffer))
+(taxy-magit-section-define-column-definer "bufler-taxy")
+
+(bufler-taxy-define-column "Name" (:max-width nil)
+  (let ((mode-annotation (when (cl-loop for fn in bufler-buffer-mode-annotate-preds
+                                        thereis (funcall fn item))
                            (propertize (concat (replace-regexp-in-string
                                                 (rx "-mode" eos) ""
-                                                (symbol-name (buffer-local-value 'major-mode buffer))
+                                                (symbol-name (buffer-local-value 'major-mode item))
                                                 t t)
                                                " ")
                                        'face 'bufler-mode)))
-        (buffer-name (buffer-name buffer))
-        (modified (when (and (buffer-file-name buffer)
-                             (buffer-modified-p buffer))
+        (buffer-name (buffer-name item))
+        (modified (when (and (buffer-file-name item)
+                             (buffer-modified-p item))
                     (propertize bufler-column-name-modified-buffer-sigil
                                 'face 'font-lock-warning-face))))
-    (concat indentation mode-annotation buffer-name modified)))
+    (concat mode-annotation buffer-name modified)))
+
+(bufler-taxy-define-column "Size" (:face bufler-size)
+  (file-size-human-readable (buffer-size item)))
+
+(bufler-taxy-define-column "Mode" (:face bufler-mode)
+  (string-remove-suffix
+   "-mode" (symbol-name (buffer-local-value 'major-mode item))))
+
+(bufler-taxy-define-column "VC" ()
+  (when (and (buffer-file-name item)
+             (or (not (file-remote-p (buffer-file-name item)))
+                 bufler-vc-remote))
+    (when (and bufler-vc-refresh
+               (vc-registered (buffer-file-name item)))
+      (with-current-buffer item
+        (vc-state-refresh (buffer-file-name item)
+                          (vc-backend (buffer-file-name item)))))
+    (pcase (vc-state (buffer-file-name item))
+      ('nil nil)
+      ((and 'edited it) (propertize (symbol-name it) 'face 'bufler-vc))
+      (it (propertize (symbol-name it) 'face 'bufler-dim)))))
+
+(bufler-taxy-define-column "Path" (:face bufler-path :max-width nil)
+  (or (buffer-file-name item)
+      (buffer-local-value 'list-buffers-directory item)
+      ""))
+
+(unless bufler-taxy-columns
+  ;; TODO: Automate this or document it.
+  (setq-default bufler-taxy-columns
+		(get 'bufler-taxy-columns 'standard-value)))
 
 ;;;; Commands
 
@@ -201,7 +214,7 @@ A buffer is special if it is not file-backed."
                                 :format-fn #'format-item
                                 :heading-face-fn #'heading-face
                                 :heading-indent bufler-taxy-level-indent
-                                :item-indent 0
+                                :item-indent bufler-taxy-item-indent
                                 args)))
       (let* ((buffers (cl-reduce #'cl-remove-if
                                  bufler-filter-buffer-fns
@@ -210,23 +223,21 @@ A buffer is special if it is not file-backed."
              (taxy (thread-last
                        (make-fn
                         :name "Bufler" :description "Buffers grouped by Bufler:"
-                        :take (bufler-taxy-take-fn keys))
+                        :take (taxy-make-take-function keys bufler-taxy-keys))
                      (taxy-fill buffers)
                      (taxy-sort* #'string< #'taxy-name)
                      (taxy-sort #'string< #'buffer-name)
                      (taxy-sort #'string<
-                                (lambda (buffer)
-                                  (symbol-name (buffer-local-value 'major-mode buffer))))))
+                       (lambda (buffer)
+                         (symbol-name (buffer-local-value 'major-mode buffer))))))
+             (taxy-magit-section-insert-indent-items nil)
              format-cons header)
         (setf format-cons (taxy-magit-section-format-items
-                           bufler-columns bufler-column-format-fns taxy)
+                           bufler-taxy-columns bufler-taxy-column-formatters taxy)
               format-table (car format-cons)
               column-sizes (cdr format-cons)
-              ;; NOTE: The first column is handled differently.
-              header (concat (format (format " %%-%ss" (cdar column-sizes)) (caar column-sizes))
-                             (cl-loop for (name . size) in (cdr column-sizes)
-                                      for spec = (format " %%-%ss" size)
-                                      concat (format spec name))))
+              header (taxy-magit-section-format-header
+                      column-sizes bufler-taxy-column-formatters))
         (when (get-buffer buffer-name)
           (kill-buffer buffer-name))
         (with-current-buffer (get-buffer-create buffer-name)
@@ -240,47 +251,6 @@ A buffer is special if it is not file-backed."
         (pop-to-buffer buffer-name)))))
 
 ;;;; Functions
-
-(defun bufler-taxy-take-fn (keys)
-  ;; TODO: Upstream this into taxy.
-  "Return a `taxy' \"take\" function for KEYS.
-Each of KEYS should be a function alias defined in
-`bufler-taxy-keys', or a list of such KEY-FNS (recursively, ad
-infinitum, approximately)."
-  (let ((macrolets (cl-loop for (name . fn) in bufler-taxy-keys
-                            collect `(,name ',fn))))
-    (cl-labels ((expand-form
-                 ;; Is using (cadr (macroexpand-all ...)) really better than `eval'?
-                 (form) (cadr (macroexpand-all
-                               `(cl-symbol-macrolet (,@macrolets)
-                                  ,form))))
-                (quote-fn
-                 (fn) (pcase fn
-                        ((pred symbolp) (expand-form fn))
-                        (`(,(and (or 'and 'or 'not) boolean) . ,(and args (map :name :keys)))
-                         ;; Well, that pcase expression isn't confusing at all...  ;)
-                         ;;  (cl-assert name t "Boolean key functions require a NAME")
-                         ;;  (cl-assert keys t "Boolean key functions require KEYS")
-                         `(lambda (buffer)
-                            (when (cl-loop for fn in ',(mapcar #'quote-fn (or keys args))
-                                           ,(pcase boolean
-                                              ('and 'always)
-                                              ('or 'thereis)
-                                              ('not 'never))
-                                           (funcall fn buffer))
-                              (or ,name ""))))
-                        (`(,(and (pred symbolp) fn)
-                           . ,(and args (guard (cl-typecase (car args)
-                                                 ((or keyword (and atom (not symbol)))
-                                                  t)))))
-                         ;; Key with args: replace with a lambda that
-                         ;; calls that key's function with given args.
-                         `(lambda (element)
-                            (,(expand-form fn) element ,@args)))
-                        ((pred listp) (mapcar #'quote-fn fn)))))
-      (setf keys (mapcar #'quote-fn keys))
-      `(lambda (item taxy)
-         (taxy-take-keyed ',keys item taxy)))))
 
 ;;;; Footer
 
